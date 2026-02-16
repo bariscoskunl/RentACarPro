79ae1b66e477dcc1d895764b0490cb389a3119fa
BARIŞ COŞKUN <bariscoskun441@gmail.com>
2026-02-16 21:12:47 +0300

    Initial MVC project setup

---
+++ RentACarPro.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentACarPro.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
