9d7bf0459c342f093df3a31c044e1b6700b866ed
BARIŞ COŞKUN <bariscoskun441@gmail.com>
2026-02-17 17:03:56 +0300

    Areas olusturuldu, Entityler eklendi.

---
+++ RentACarPro.Mvc/Entities/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace RentACarPro.Mvc.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; } 
+        public string Brand { get; set; } 
+        public string Model { get; set; } 
+        public int Year { get; set; }    
+        public decimal DailyPrice { get; set; } 
+        public string ImageUrl { get; set; } 
+        public bool IsActive { get; set; } = true; 
+
+        // BU ARABA KİME AİT? 
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
